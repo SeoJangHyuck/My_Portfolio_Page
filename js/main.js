73390d213318db3da23664d957cf207ac4b3b468
SeoJangHyuck <sjh938606@gmail.com>
2019-01-10 19:47:08 +0900

    no message

--- js/main.js
+++ js/main.js
@@ -4,9 +4,18 @@ $(function(){
         anchors:["m_home","m_profile","m_ability","m_portfolio"],
         afterLoad: function(anchorLink, index){
             $("#m_gnb_wrap>ul>li").eq(index-1).addClass("m_gnb_active");
+            if(index==2){
+                $(".m_profile_title>img, .m_profile_foot>img").addClass("m_profile_active");
+            };
+            if(index==3){
+            };
         },
         onLeave: function(index, nextIndex, direction){
             $("#m_gnb_wrap>ul>li").removeClass("m_gnb_active");
         }
+    });
+
+    $(".m_slider").bxSlider({
+        
     });
 }); //End
