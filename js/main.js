5349e071d94e97e6ae2a0546f56d9783a35ee498
SeoJangHyuck <sjh938606@gmail.com>
2019-01-14 16:58:25 +0900

    no message

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $(function(){
+    var c = 0;
+    
     // Fullpage
     $("#m_content_box").fullpage({
         menu: "#m_gnb_wrap",
@@ -10,29 +12,19 @@ $(function(){
             };
             if(index==3){
                 $(".m_camera_film, .m_ability_foot>img").addClass("m_ability_active");
-                $(".m_camera_film").each(function(){
-                    // var flimTop = setInterval(function(){
-                    //     $(this).css("top");
-                    // },50);
-                    //console.log(flimTop);
-                    //var flimTopNum = parseInt(flimTop);
-                    //if(flimTopNum == -2){
-                    //    clearInterval(flimTop);
-                    //}
-                    // if(flimTopNum >=- 180){
-                    //     var abilityPercent = $(this).children("p").text();
-                    //     var abilityPercentNum = parseInt(abilityPercent);
-                    //     var countA = 0;
-                    //     function changPercent(){
-                    //         countA++;
-                    //         if(abilityPercentNum>=countA){
-                    //             return;
-                    //         }
-                    //         $(this).children("p").text(countA+"%");
-                    //         setTimeout(function(){flimEvent();},50);
-                    //     }
-                    // }
-                });
+                ct = setInterval(changeT,100);
+                changeT();
+                function changeT(){
+                    c++;
+                    if(c<=40){
+                        $(".m_camera_film").each(function(){
+                            cp = $(this).attr("data-num");
+                            $(this).find("p").text(Math.ceil(cp*c/40)+"%");
+                        });
+                    }else{
+                        clearInterval(ct);
+                    }
+                }
             };
             if(index==4){
                 $(".m_portfolio_foot>img").addClass("m_portfolio_active");
