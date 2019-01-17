c38a4cb41aacc48cd11c2ae3213bf7b67f176d9a
SeoJangHyuck <sjh938606@gmail.com>
2019-01-17 18:59:03 +0900

    no message

--- js/common.js
+++ js/common.js
@@ -1,4 +1,12 @@
 $(function(){
-    
-    
+    // Mobile Menu GNB
+    var mouseCount = 0;
+    $("#m_mgnb_wrap>button").click(function(){
+        mouseCount++;
+        if(mouseCount%2 == 1){
+            $("#m_mgnb_wrap, #m_mgnb_wrap>ul").addClass("m_gnb_active");
+        }else{
+            $("#m_mgnb_wrap, #m_mgnb_wrap>ul").removeClass("m_gnb_active");
+        }
+    });
 }); //End
--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ $(function(){
         anchors:["m_home","m_profile","m_ability","m_portfolio"],
         afterLoad: function(anchorLink, index){
             $("#m_gnb_wrap>ul>li").eq(index-1).addClass("m_gnb_active");
+            $("#m_mgnb_wrap>ul>li").eq(index-1).addClass("m_gnb_active");
             if(index==2){
                 $(".m_profile_title>img, .m_profile_foot>img").addClass("m_profile_active");
             };
@@ -31,24 +32,35 @@ $(function(){
             }
         },
         onLeave: function(index, nextIndex, direction){
+            $("#m_gnb_wrap>ul>li>a").blur();
             $("#m_gnb_wrap>ul>li").removeClass("m_gnb_active");
+            $("#m_mgnb_wrap>ul>li>a").blur();
+            $("#m_mgnb_wrap>ul>li").removeClass("m_gnb_active");
         }
     });
 
     // bxSlider
     $(".m_slider").bxSlider({
-        maxSlides: 2,
+        maxSlides: 3,
         minSlides: 1,
         moveSlides: 1
     });
 
     // Profile
+    $(".m_slider_page3 a").mouseover(function(){
+        $(this).parent().next().stop().fadeIn(400);
+    });
+    $(".m_slider_page3 a").mouseout(function(){
+        $(this).parent().next().stop().fadeOut(400);
+    });
     $(".m_slider_page3>div>p>em>button").click(function(){
+        $("#m_hd").addClass("m_header_active");
         var porModal = $(this).attr("data-pm");
         $("."+porModal).addClass("m_profile_active");
         $(".m_profile_modal").fadeIn(400);
     });
     $(".m_profile_modal>div>button").click(function(){
+        $("#m_hd").removeClass("m_header_active");
         $(this).parent().removeClass("m_profile_active");
         $(".m_profile_modal").fadeOut(400);
     });
@@ -63,11 +75,13 @@ $(function(){
         $(".m_portfolio_box>img").eq(1).removeClass("m_portfolio_active");
     });
     $(".m_portfolio_index>li>a").click(function(){
+        $("#m_hd").addClass("m_header_active");
         var portModal = $(this).attr("data-pm");
         $("."+portModal).addClass("m_portfolio_active");
         $(".m_portfolio_modal").fadeIn(400);
     });
     $(".m_pm_box>div>button").click(function(){
+        $("#m_hd").removeClass("m_header_active");
         $(this).parent().removeClass("m_portfolio_active");
         $(".m_portfolio_modal").fadeOut(400);
     });
